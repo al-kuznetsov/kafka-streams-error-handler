5abde3bf2accb0e2e4cf149bb090fd56b249429f
alkuznetsov <aakuznetsof@yandex.ru>
2024-04-08 22:47:44 +0500

    Refine

--- src/main/java/al/Main.java
+++ src/main/java/al/Main.java
@@ -2,6 +2,7 @@ package al;
 
 import java.util.Properties;
 
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -19,12 +20,17 @@ public class Main {
 
         Properties settings = new Properties();
         settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-error-handler");
-        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
         // settings.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG,
         // StreamsConfig.EXACTLY_ONCE_V2);
 
         settings.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         settings.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        settings.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        settings.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+        settings.put(SaslConfigs.SASL_JAAS_CONFIG,
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"nana\" password=\"nana-secret\";");
 
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         TopologyBuilder topologyBuilder = new TopologyBuilder();
